b73c967593c78761c0e6dc72bc4b65c53df7af02
Jayesh Choudhary <choudharyjayesh668@gmail.com>
2026-05-19 00:05:36 +0530

    Complete Login Authentication

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const path = require("path");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect("mongodb://127.0.0.1:27017/NoteNest")
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/", (req, res) => {
+    res.render("signup.ejs",{
+        error: null
+    });
+});
+
+app.post("/signup", async (req, res) => {
+    const email=req.body.email;
+    const password=req.body.password;
+    const name=req.body.name;
+    const confirmpassword = req.body.confirmpassword;
+    console.log("Email:"+email);
+    console.log("Password:"+password);
+    console.log("Name:"+name);
+    console.log("confirm Password:"+confirmpassword);
+    if(password !== confirmpassword){
+        return res.render("signup.ejs", {
+            error: "Passwords do not match"
+        });
+    }
+    const newUser=new User({
+        name: name,
+        email: email,
+        password: password,
+    });
+    try {
+        await newUser.save();
+        console.log("User saved");
+        res.render("login.ejs",{
+            error: null
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Error saving user");
+    }
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs",{
+        error: null
+    });
+});
+
+app.post("/login", async (req, res) => {
+    const email=req.body.email;
+    const password=req.body.password;
+    try{
+        const foundUser= await User.findOne({email:email});
+        if(!foundUser){
+            console.log(foundUser);
+            console.log("User not found");
+            return res.render("login.ejs",{
+                error:"User not found"
+            });
+        }
+        if(foundUser.password !== password){
+            return res.render("login.ejs",{
+                error:"Password Didnt Match"
+            });
+        }
+        res.render("NodeNest.ejs");
+    }catch(err){
+        console.log(err);
+        res.render("login.ejs",{
+            error:"SomeThing Went Wrong"
+        });
+    }
+});
+
+app.listen(8080, () => {
+    console.log("http://localhost:8080");
+});
